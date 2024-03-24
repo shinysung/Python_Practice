b186e6694ec3dbb3ca6394ff121f5ecbf2a6f290
shinysung <164750793+shinysung@users.noreply.github.com>
2024-03-24 22:04:27 +0900

    Update fibonacci_sequence.py

--- fibonacci_sequence.py
+++ fibonacci_sequence.py
@@ -2,13 +2,13 @@
 # Recurrence relation : F(n) = F(n-1) + F(n-2)
 
 
-previous = 0 #F(n-1)
-current = 1 #F(n)
+previous = 0 # F(n-1)
+current = 1 # F(n)
 i = 1 #term
 
 while i <= 50:
     print(current)
-    temp = previous #F(n-2)
+    temp = previous # F(n-2)
     previous = current
     current = current + temp
     i += 1
