49003450f252d1ae3e58960a79707e25a995aea7
shinysung <164750793+shinysung@users.noreply.github.com>
2024-03-24 22:07:13 +0900

    Add files via upload

---
+++ fibonacci_sequence.py
@@ -0,0 +1,14 @@
+# To print the first 50 terms of the Fibonacci sequence sequentially
+# Recurrence relation : F(n) = F(n-1) + F(n-2)
+
+
+previous = 0 # F(n-1)
+current = 1 # F(n)
+i = 1 #term
+
+while i <= 50:
+    print(current)
+    temp = previous # F(n-2)
+    previous = current
+    current = current + temp
+    i += 1
