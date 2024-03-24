c2e092b06bbe4817debcee3c33803e0a236525a1
shinysung <164750793+shinysung@users.noreply.github.com>
2024-03-24 22:39:22 +0900

    Add files via upload

---
+++ dict_vocab.py
@@ -0,0 +1,16 @@
+# To make the dictionary for vocabulary
+
+vocab = { "sanitizer": "살균제",
+          "ambition": "야망",
+          "conscience": "양심",
+          "civilization": "문명"
+          }
+
+print(vocab)
+
+# Add more items
+
+vocab["privilege"] = "특권"
+vocab["principle"] = "원칙"
+
+print(vocab)
