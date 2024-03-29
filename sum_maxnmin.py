1bded2e8273cbe8e9003ec1151da8e4cac9b1e79
shinysung <164750793+shinysung@users.noreply.github.com>
2024-03-29 23:33:17 +0900

    Add files via upload

---
+++ sum_maxnmin.py
@@ -0,0 +1,12 @@
+# To sum all the numbers of a given array, except the highest and the lowest element.
+
+
+def sum_array(arr):
+    if arr == None or len(arr) < 3:
+        return 0
+
+    return sum(arr) - max(arr) - min(arr)
+
+print(sum_array([6, 2, 1, 8, 10]))
+
+
